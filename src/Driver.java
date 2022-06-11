c526980b87faf3ada75c1dd9d161be6f38fad2c3
Miko Tansingco <mig.f.c.tan@gmail.com>
2022-06-11 17:29:20 +0800

    Added user input for numPassengers, maxCapacity, and numCars

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
 public class Driver {
     public static void main(String[] args) {
         int numPassengers, maxCapacity, numCars;
+        Scanner sc = new Scanner(System.in);
 
         // For testing
         numPassengers = 6;
         maxCapacity = 2;
         numCars = 2;
+
+        System.out.print("Number of Passengers: ");
+        numPassengers = sc.nextInt();
+
+        System.out.println();
+
+        System.out.print("Car Capacity: ");
+        maxCapacity = sc.nextInt();
+
+        System.out.println();
+
+        System.out.print("Number of Cars: ");
+        numCars = sc.nextInt();
+
+        System.out.println();
+
+
 
         Semaphore slotsAvailable, loadZone, unloadZone, boardFinished, unboardFinished, slotsTaken;
         slotsAvailable = new Semaphore(0);
@@ -26,5 +45,7 @@ public class Driver {
             Thread thread = new Thread(new Car(i, maxCapacity, unloadZone, loadZone, slotsAvailable, boardFinished, unboardFinished, slotsTaken));
             thread.start();
         }
+
+        return;
     }
 }
